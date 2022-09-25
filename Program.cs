4433fdd6e6778138fe0ab5ac73675730fae3f29d
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2022-09-26 02:12:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Restaraunt.Booking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+                var rest = new Restaraunt.Booking.Restaraunt();
+                while (true)
+                {
+                    await Task.Delay(10000);
+
+                    Console.WriteLine("Привет! Желаете забронировать столик?");
+
+                    var stopWatch = new Stopwatch();
+                    stopWatch.Start();
+
+                    rest BookFreeTabLeAsync(countOfPersons:1);
+                    Console.WriteLine("Спасибо за Ваше обращение!");
+                    stopWatch.Stop();
+                    var ts:TimeSpan = stopWatch.Elapsed;
+                    Console.WriteLine($"{ts.Seconds:00}:{ts.Milliseconds:00}");
+
+                }
+        }
+    }
+}
