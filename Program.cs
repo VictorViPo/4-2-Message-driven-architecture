dc773b231bc6a0b5d589292472390e01b328a06b
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2022-10-20 19:29:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Извините! Д/З будет здесь 21.10.2022 08:00 простите немогу загрузить раньше.");
+        }
+    }
+}
