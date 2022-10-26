13f9da3425656f55540e2b1f91a1b0987fd7de94
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2022-10-27 01:35:37 +0300

    Add files via upload

---
+++ Restaurant.Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MassTransit;
+
+namespace Restaurant.Booking
+{
+    public class RestaurantBooking : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; } //для всех сообщений
+
+        public int CurrentState { get; set; } //текущее состояние
+
+        public Guid OrderId { get; set; } //заказа/бронирования
+
+        public Guid ClientId { get; set; } //клиент
+
+        public int ReadyEventStatus { get; set; } //события
+
+        public Guid? ExpirationId { get; set; } //пометка о прокисшей заявке
+    }
+}
